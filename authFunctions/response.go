0cb1fa66f5679cd5d0f4d94a04c7cbd322811449
Ryuku Hisasue <38874137+Ryuku-Hisa@users.noreply.github.com>
2020-07-29 09:51:38 +0900

    for review

---
+++ authFunctions/response.go
@@ -0,0 +1,17 @@
+package authFunctions
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func errorInResponse(w http.ResponseWriter, status int, error Error) {
+	w.WriteHeader(status) //HTTP status コードが入る
+	json.NewEncoder(w).Encode(error)
+	return
+}
+
+func responseByJSON(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(data)
+	return
+}
---
+++ authFunctions/structs.go
@@ -0,0 +1,16 @@
+package authFunctions
+
+type User struct {
+	// 大文字だと Public 扱い
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWT struct {
+	Token string `json:"token"`
+}
+
+type Error struct {
+	Message string `json:"message"`
+}
